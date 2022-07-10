542505f5ed31604dfb52d0cbac9b2c0745cf986c
Gustavo <gustavosm994@gmail.com>
2022-07-10 11:11:03 -0300

    create automations page

--- src/pages/automations/index.tsx
+++ src/pages/automations/index.tsx
@@ -1,74 +1,124 @@
+import { useEffect, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import {
+  Box,
   Button,
-  Checkbox,
   Flex,
   Heading,
+  HStack,
   Icon,
-  Table,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tr,
+  ModalFooter,
+  useDisclosure,
+  useToast,
 } from '@chakra-ui/react';
-import Link from 'next/link';
-import { RiAddLine, RiPencilLine } from 'react-icons/ri';
-import { Layout } from '../../components/Layout';
+import { RiAddLine } from 'react-icons/ri';
+
+import { Input } from '@components/Form/Input';
+import { Layout } from '@components/Layout';
+import { Modal } from '@components/Overlay';
+import { Table } from '@components/pages/automations/Table';
+
+import { useServie } from '@context/service';
+
+type IAutomations = {
+  id: string;
+  status: string;
+  schedule: string;
+}
+
+type CreateAutomationFormData = {
+  status: string;
+  schedule: string;
+}
 
 export default function Automations() {
+  const service = useServie();
+  const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { register, handleSubmit, resetField } = useForm<CreateAutomationFormData>();
+  const [automations, setAutomations] = useState<IAutomations[]>([]);
+
+  const fetchAutomations = async () => {
+    await service.automation.list({
+      onSuccess: (data) => setAutomations(data),
+      onError: (error) => console.log('error', error),
+    });
+  };
+  fetchAutomations();
+
+  useEffect(() => {
+    fetchAutomations();
+  }, []);
+
+  const handleCreateAutomation: SubmitHandler<CreateAutomationFormData> = async (values) => {
+    const { schedule, status } = values;
+    await service.automation.create({
+      data: { schedule, status },
+      onSuccess: () => {
+        toast({
+          title: 'Sucesso',
+          description: 'Automação criada com sucesso!',
+          status: 'success',
+          duration: 4000,
+          isClosable: true,
+          position: 'top-right',
+        });
+        resetField('status');
+        resetField('schedule');
+        onClose();
+      },
+      onError: () => {
+        toast({
+          title: 'Error',
+          description: 'Erro no cadastro da automação!',
+          status: 'error',
+          duration: 4000,
+          isClosable: true,
+          position: 'top-right',
+        });
+        resetField('status');
+        resetField('schedule');
+        onClose();
+      },
+    });
+  };
+
   return (
     <Layout>
       <Flex mb="8" justify="space-between" align="center">
         <Heading size="lg" fontWeight="normal">Automações</Heading>
-
-        <Link href="/" passHref>
-          <Button
-            as="a"
-            size="sm"
-            fontSize="small"
-            colorScheme="green"
-            leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-          >
-            Criar nova
-          </Button>
-        </Link>
+        <Button
+          size="sm"
+          fontSize="small"
+          colorScheme="purple"
+          leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+          onClick={onOpen}
+        >
+          Criar nova
+        </Button>
       </Flex>
-      <Table colorScheme="whiteAlpha">
-        <Thead>
-          <Tr>
-            <Th px={['4', '4', '6']} color="gray.300" width="8">
-              <Checkbox colorScheme="purple" />
-            </Th>
-            <Th>Status</Th>
-            <Th>Data de cadastro</Th>
-            <Th width="8">Ação</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td px={['4', '4', '6']}>
-              <Checkbox colorScheme="purple" />
-            </Td>
-            <Td>
-              <Text fontWeight="bold">Launch time</Text>
-            </Td>
-            <Td>04 de Abril, 2022</Td>
-            <Td>
-              <Button
-                as="a"
-                size="sm"
-                fontSize="small"
-                colorScheme="orange"
-                leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-              >
-                Editar
-              </Button>
+      <Table automations={automations} handleListAutomations={fetchAutomations} />
 
-            </Td>
-          </Tr>
-        </Tbody>
-      </Table>
+      <Modal
+        title="Nova automação"
+        isOpen={isOpen}
+        onClose={onClose}
+        componentFooter={(
+          <ModalFooter>
+            <Box as="form" onSubmit={handleSubmit(handleCreateAutomation)}>
+              <HStack>
+                <Button colorScheme="purple" type="submit">
+                  Salvar
+                </Button>
+                <Button colorScheme="red" onClick={onClose}>Cancelar</Button>
+              </HStack>
+            </Box>
+          </ModalFooter>
+        )}
+      >
+        <Input label="status" {...register('status')} />
+        <Input label="schedule" type="time" {...register('schedule')} />
+      </Modal>
     </Layout>
   );
 }
