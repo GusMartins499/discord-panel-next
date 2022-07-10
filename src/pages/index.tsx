e3ba60e845b07e830a1ae9b1a94cd30a7015aba3
Gustavo <gustavosm994@gmail.com>
2022-07-10 11:10:44 -0300

    refactor dashboard page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,36 @@
-import { GetServerSideProps } from 'next';
-import axios from 'axios';
+import { useState } from 'react';
 import {
   Box, Heading, SimpleGrid, Text,
 } from '@chakra-ui/react';
-import { getSession, useSession } from 'next-auth/react';
-import { Layout } from '../components/Layout';
 
-type DiscordGuilds = {
+import { Layout } from '@components/Layout';
+
+import { useFetch } from '@hooks/useFetch';
+
+type IDiscordGuilds = {
   id: string;
   name: string;
 }
 
-interface IDiscordGuilds {
-  discordGuilds: DiscordGuilds[];
-}
+export default function Dashboard() {
+  const [discordGuilds, setDiscordGuilds] = useState<IDiscordGuilds[]>([]);
 
-export default function Dashboard({ discordGuilds }: IDiscordGuilds) {
-  const { status } = useSession();
+  useFetch({
+    url: 'https://discord.com/api/users/@me/guilds',
+    onSuccess: (response) => setDiscordGuilds(response),
+    onError: () => console.error('error'),
+  });
+
   return (
     <Layout>
       <Heading size="lg" fontWeight="normal">Seus servidores</Heading>
-      {status === 'authenticated' && discordGuilds ? (
-        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={4} mt="4">
-          {discordGuilds.map((guild) => (
-            <Box w="100%" h="100%" bg="gray.700" key={guild.id} borderRadius="4">
-              <Text fontSize="1.1rem" fontWeight="bold" p="1" textAlign="center">{guild.name}</Text>
-            </Box>
-          ))}
-        </SimpleGrid>
-      ) : (
-        <Heading size="lg" fontWeight="normal" mt="8">Ops .. Parece que você não está autenticado</Heading>
-      )}
+      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={4} mt="4">
+        {discordGuilds?.map((guild) => (
+          <Box w="100%" h="100%" bg="gray.700" key={guild.id} borderRadius="4">
+            <Text fontSize="1.1rem" fontWeight="bold" p="1" textAlign="center">{guild.name}</Text>
+          </Box>
+        ))}
+      </SimpleGrid>
     </Layout>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context);
-  if (!session) {
-    return {
-      props: {
-        discordGuilds: [],
-      },
-    };
-  }
-  const discordGuilds = await axios.get<IDiscordGuilds[]>('https://discord.com/api/users/@me/guilds', {
-    headers: {
-      Authorization: `Bearer ${session?.accessToken}`,
-    },
-  });
-  return {
-    props: {
-      discordGuilds: discordGuilds?.data || null,
-    },
-  };
-};
